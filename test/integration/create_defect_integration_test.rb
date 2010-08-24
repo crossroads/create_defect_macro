0958dae4b95c5d5d2a5694e0b4dad697c55e34cc
Nathan B <nathan.f77@gmail.com>
2010-08-24 16:49:36 +0800

    Fixed integration test warning

--- test/integration/create_defect_integration_test.rb
+++ test/integration/create_defect_integration_test.rb
@@ -7,24 +7,28 @@ require File.dirname(__FILE__) + '/integration_test_helper.rb'
 # to the mingle data directory on your installation e.g.
 # basic_authentication_enabled: true.
 
-class CreateDefectMacroIntegrationTest < Test::Unit::TestCase
+#class CreateDefectMacroIntegrationTest < Test::Unit::TestCase
 
-  PROJECT_RESOURCE = 'http://username:password@your.mingle.server:port/lightweight_projects/your_project_identifier.xml'
-  ANOTHER_PROJECT_RESOURCE = 'http://username:password@your.mingle.server:port/lightweight_projects/another_project_identifier.xml'
-
-  # Integration tests are not necessary, as we are mainly using javascript.
+#  PROJECT_RESOURCE = 'http://username:password@your.mingle.server:port/lightweight_projects/your_project_identifier.xml'
+#  ANOTHER_PROJECT_RESOURCE = 'http://username:password@your.mingle.server:port/lightweight_projects/another_project_identifier.xml'
 
 #  def test_macro_contents
 #    create_defect = CreateDefectMacro.new(nil, project(PROJECT_RESOURCE), nil)
 #    result = create_defect.execute
 #    assert result
 #  end
-#
+
 #  def test_macro_contents_with_a_project_group
 #    create_defect = CreateDefectMacro.new(nil, [project(PROJECT_RESOURCE), project(ANOTHER_PROJECT_RESOURCE)], nil)
 #    result = create_defect.execute
 #    assert result
 #  end
 
-end
+#end
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Integration tests are not necessary, as we are mainly using javascript,
+#                   and only calling 'THIS CARD'.
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
