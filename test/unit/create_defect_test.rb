3295cdb9213267d58092a363d257c28912d9a80a
Nathan Broadbent <nathan.f77@gmail.com>
2011-06-01 02:07:27 +0800

    Fixed tests

--- test/unit/create_defect_test.rb
+++ test/unit/create_defect_test.rb
@@ -6,9 +6,9 @@ class CreateDefectMacroTest < Test::Unit::TestCase
 
   def test_macro_contents
     Mingle::Project.any_instance.stubs(:execute_mql).returns([{'number' => "564"}])
-    create_defect = CreateDefectMacro.new(nil, project(FIXTURE), nil)
+    create_defect = CreateDefectMacro.new(nil, project(FIXTURE), project(FIXTURE).team.first)
     result = create_defect.execute
-    assert result.include?("createDefect('gh', 564)")
+    assert result.include?("createDefect('gh', 'groucho', 564)")
   end
 
 end
